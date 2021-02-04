c092159ff45c82d6564a66a3eb17c6689ccc9cf5
m-arezoumandan <mortezaarezoomandan@gmail.com>
2021-02-04 16:37:40 +0330

    Added distance matrix

--- data_smoothing.py
+++ data_smoothing.py
@@ -1,13 +1,19 @@
 import scipy.signal
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def smoothing(daily_cases_norm):
     smoothed = []
     for col in daily_cases_norm.T:
-        # window size 11, polynomial order 3
-        yhat = scipy.signal.savgol_filter(col, 11, 3)
+        # window size 35, polynomial order 3
+        yhat = scipy.signal.savgol_filter(col, 35, 3)
         smoothed.append(yhat)
 
+    # x = np.linspace(1, 300, 300)
+    # yhat = scipy.signal.savgol_filter(daily_cases_norm.T[90], 35, 3)  # window size 51, polynomial order 3
+    # plt.plot(x, daily_cases_norm.T[90])
+    # plt.plot(x, yhat, color='red')
+    # plt.show()
     smoothed_transpose = np.array(smoothed).transpose()
     return smoothed_transpose
--- main.py
+++ main.py
@@ -1,14 +1,47 @@
 import data_cleaning as dc
-import data_normalization as dn
+from sklearn.preprocessing import MinMaxScaler
 import data_smoothing as ds
 import pandas as pd
-
+from tslearn.metrics import dtw
+import numpy as np
+import matplotlib.pyplot as plt
 
 data = dc.cleaning()
 daily_cases = pd.DataFrame(data)
-#daily_cases_trans = daily_cases ** 0.5
-daily_cases_norm = dn.normalizing(daily_cases.values)
-daily_cases_norm.columns = daily_cases.columns
-daily_cases_smoothed = pd.DataFrame(ds.smoothing(daily_cases_norm.values))
+# daily_cases_trans = daily_cases ** 0.5
+daily_cases_norm = MinMaxScaler().fit_transform(daily_cases.values)
+daily_cases_smoothed = pd.DataFrame(ds.smoothing(daily_cases_norm))
 daily_cases_smoothed.columns = daily_cases.columns
-print(daily_cases_smoothed)
+
+# plotting different countries patterns and their dtw score
+# dtw_score = dtw(daily_cases_smoothed["Italy"], daily_cases_smoothed["China"])
+# x = np.linspace(1, 300, 300)
+# plt.plot(x, daily_cases_smoothed["China"], label="China")
+# plt.plot(x, daily_cases_smoothed["Italy"], color='red', label="Italy")
+# plt.legend()
+# dtw_str = "DTW Score = " + str(dtw_score)
+# plt.title(dtw_str)
+# plt.show()
+
+# calculate the distance matrix
+num_countries = len(daily_cases_smoothed.columns)
+distance_matrix = np.zeros((num_countries, num_countries))
+for index_i, i in enumerate(daily_cases_smoothed.values.T):
+    for index_j, j in enumerate(daily_cases_smoothed.values.T):
+        distance_matrix[index_i, index_j] = dtw(i, j)
+print("Distance Matrix:")
+print(distance_matrix)
+
+'''
+compute the matrix of distances : DONE
+
+Hopkins statistics
+
+Use different clustering methods 
+
+Evaluate all clustering methods used
+
+Visualization
+
+Final analysis
+'''
