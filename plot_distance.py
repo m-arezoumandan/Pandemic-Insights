ebdae9f9439c3f6f8a1d00e777df229fb251979e
m-arezoumandan <mortezaarezoomandan@gmail.com>
2021-02-10 22:54:12 +0330

    Changed plot_distance to plot clusters

--- plot_distance.py
+++ plot_distance.py
@@ -1,15 +1,18 @@
 from sklearn.manifold import MDS
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 
 def plot_distance(distance_matrix, labels):
     mds = MDS(n_components=2, dissimilarity="precomputed")
     results = mds.fit(distance_matrix)
-    countries = labels
+    my_colors = ['red', 'green', 'blue']
     coords = results.embedding_
     fig = plt.figure(figsize=(12, 10))
     plt.subplots_adjust(bottom=0.1)
-    plt.scatter(coords[:, 0], coords[:, 1])
-    for label, x, y in zip(countries, coords[:, 0], coords[:, 1]):
-        plt.annotate(label, xy=(x, y), xytext=(-10, 10), textcoords='offset points')
+    plt.scatter(coords[:, 0], coords[:, 1], c=labels)
+    #for label, x, y in zip(labels, coords[:, 0], coords[:, 1]):
+    #    plt.annotate("", xy=(x, y), xytext=(-10, 10), textcoords='offset points')
+
     plt.show()
