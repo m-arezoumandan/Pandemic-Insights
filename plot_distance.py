97e23754127d5ea47e93009f96ba966ec3e7465e
m-arezoumandan <mortezaarezoomandan@gmail.com>
2021-02-13 00:16:05 +0330

    Changed plot_distance to plot clusters with labels

--- plot_distance.py
+++ plot_distance.py
@@ -3,8 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-
-def plot_distance(distance_matrix, labels):
+def plot_distance(distance_matrix, labels, countries):
     mds = MDS(n_components=2, dissimilarity="precomputed")
     results = mds.fit(distance_matrix)
     my_colors = ['red', 'green', 'blue']
@@ -12,7 +11,7 @@ def plot_distance(distance_matrix, labels):
     fig = plt.figure(figsize=(12, 10))
     plt.subplots_adjust(bottom=0.1)
     plt.scatter(coords[:, 0], coords[:, 1], c=labels)
-    #for label, x, y in zip(labels, coords[:, 0], coords[:, 1]):
-    #    plt.annotate("", xy=(x, y), xytext=(-10, 10), textcoords='offset points')
+    for country, x, y in zip(countries, coords[:, 0], coords[:, 1]):
+        plt.annotate(country, xy=(x, y), xytext=(-10, 10), textcoords='offset points')
 
     plt.show()
